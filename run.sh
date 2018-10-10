3ee3d9686b405fafb3ab32a407a879bcb329e9f1
Ramnath Nayak <ramnathnayakpr@gmail.com>
2018-10-10 23:44:29 +0100

    Debugs

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-set -x
+#!/bin/bash
+echo Started K8s-deploy
 echo $K8S_DEPLOY_URL
 echo $K8S_DEPLOY_SERVER
 echo $K8S_DEPLOY_TOKEN
@@ -6,3 +7,4 @@ echo $K8S_DEPLOY_DEPLOYMENT
 echo $K8S_DEPLOY_OLD_IMAGE
 echo $K8S_DEPLOY_NEW_IMAGE
 echo $K8S_DEPLOY_TYPE
+echo Finished K8s-deploy
