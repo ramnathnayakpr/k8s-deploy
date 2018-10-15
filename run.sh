3c4b9a41b52497b8237ec705d2697aac4bd9745c
Ramnath Nayak <ramnathnayakpr@gmail.com>
2018-10-15 23:02:36 +0100

    Changed kubectl prefix

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ echo Started K8s-deploy
 if [ ${WERCKER_K8S_DEPLOY_TYPE} = "rolling-update" ]
 then
   echo Attempting Rolling update for ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
-  ${WERCKER_OUTPUT_DIR}/kubectl --server=${WERCKER_K8S_DEPLOY_SERVER} --token=${WERCKER_K8S_DEPLOY_TOKEN} --insecure-skip-tls-verify set image deployment.apps/${WERCKER_K8S_DEPLOY_DEPLOYMENT} ${WERCKER_K8S_DEPLOY_OLD_IMAGE}=${WERCKER_K8S_DEPLOY_NEW_IMAGE}
-  ${WERCKER_OUTPUT_DIR}/kubectl rolliout status ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
+  kubernetes/client/bin/kubectl --server=${WERCKER_K8S_DEPLOY_SERVER} --token=${WERCKER_K8S_DEPLOY_TOKEN} --insecure-skip-tls-verify set image deployment.apps/${WERCKER_K8S_DEPLOY_DEPLOYMENT} ${WERCKER_K8S_DEPLOY_OLD_IMAGE}=${WERCKER_K8S_DEPLOY_NEW_IMAGE}
+  kubernetes/client/bin/kubectl rolliout status ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
 elif [ ${WERCKER_K8S_DEPLOY_TYPE} = "blue-green" ]
 then
   echo Attempting blue-green update for ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
