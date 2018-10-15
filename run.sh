1d0a0a692673c6b61cd2f50a0594b50879164ea6
Ramnath Nayak <ramnathnayakpr@gmail.com>
2018-10-15 23:59:44 +0100

    Removed latest to use tag passed in

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo Started K8s-deploy
 if [ ${WERCKER_K8S_DEPLOY_TYPE} = "rolling-update" ]
 then
   echo Attempting Rolling update for ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
-  /mnt/k8s-deploy*/kubectl --server=${WERCKER_K8S_DEPLOY_SERVER} --token=${WERCKER_K8S_DEPLOY_TOKEN} --insecure-skip-tls-verify set image deployment.apps/${WERCKER_K8S_DEPLOY_DEPLOYMENT} ${WERCKER_K8S_DEPLOY_OLD_IMAGE}=${WERCKER_K8S_DEPLOY_NEW_IMAGE}:latest
+  /mnt/k8s-deploy*/kubectl --server=${WERCKER_K8S_DEPLOY_SERVER} --token=${WERCKER_K8S_DEPLOY_TOKEN} --insecure-skip-tls-verify set image deployment.apps/${WERCKER_K8S_DEPLOY_DEPLOYMENT} ${WERCKER_K8S_DEPLOY_OLD_IMAGE}=${WERCKER_K8S_DEPLOY_NEW_IMAGE}
   /mnt/k8s-deploy*/kubectl --server=${WERCKER_K8S_DEPLOY_SERVER} --token=${WERCKER_K8S_DEPLOY_TOKEN} --insecure-skip-tls-verify rollout status deployment ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
 elif [ ${WERCKER_K8S_DEPLOY_TYPE} = "blue-green" ]
 then
