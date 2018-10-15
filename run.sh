2a89b11adf1f66232d2483800344f86913f98e42
Ramnath Nayak <ramnathnayakpr@gmail.com>
2018-10-15 23:22:01 +0100

    Added token and master for rollout

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [ ${WERCKER_K8S_DEPLOY_TYPE} = "rolling-update" ]
 then
   echo Attempting Rolling update for ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
   /mnt/k8s-deploy*/kubectl --server=${WERCKER_K8S_DEPLOY_SERVER} --token=${WERCKER_K8S_DEPLOY_TOKEN} --insecure-skip-tls-verify set image deployment.apps/${WERCKER_K8S_DEPLOY_DEPLOYMENT} ${WERCKER_K8S_DEPLOY_OLD_IMAGE}=${WERCKER_K8S_DEPLOY_NEW_IMAGE}
-  /mnt/k8s-deploy*/kubectl rollout status deployment ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
+  /mnt/k8s-deploy*/kubectl --server=${WERCKER_K8S_DEPLOY_SERVER} --token=${WERCKER_K8S_DEPLOY_TOKEN} --insecure-skip-tls-verify rollout status deployment ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
 elif [ ${WERCKER_K8S_DEPLOY_TYPE} = "blue-green" ]
 then
   echo Attempting blue-green update for ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
