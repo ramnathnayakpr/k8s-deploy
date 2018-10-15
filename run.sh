7f6feb80ff5799b22bdd667cb6d030234ac1c345
Ramnath Nayak <ramnathnayakpr@gmail.com>
2018-10-15 22:20:45 +0100

    Initial version with rolling update

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 echo Started K8s-deploy
-echo $K8S_DEPLOY_URL
-echo $K8S_DEPLOY_SERVER
-echo $K8S_DEPLOY_TOKEN
-echo $K8S_DEPLOY_DEPLOYMENT
-echo $K8S_DEPLOY_OLD_IMAGE
-echo $K8S_DEPLOY_NEW_IMAGE
-echo $K8S_DEPLOY_TYPE
-echo Finished K8s-deploy
+
+if ${WERCKER_K8S_DEPLOY_TYPE} -eq "rolling-update"
+then
+  echo Attempting Rolling update for ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
+  kubectl --server=${WERCKER_K8S_DEPLOY_SERVER} --token=${WERCKER_K8S_DEPLOY_TOKEN} --insecure-skip-tls-verify set image deployment.apps/${WERCKER_K8S_DEPLOY_DEPLOYMENT} ${WERCKER_K8S_DEPLOY_OLD_IMAGE}=${WERCKER_K8S_DEPLOY_NEW_IMAGE}
+  kubectl rolliout status ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
+elif ${WERCKER_K8S_DEPLOY_TYPE} -eq "blue-green"
+then
+  echo Attempting blue-green update for ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
+elif ${WERCKER_K8S_DEPLOY_TYPE} -eq "canary"
+then
+  echo Attempting canary update for ${WERCKER_K8S_DEPLOY_DEPLOYMENT}
+fi
